35856d4b5d3028cf8ec900bcc8eb68d4273394ff
ravimokka <ravi.info1990@gmail.com>
2019-12-13 14:45:46 +0530

    sqd project

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ravimokka/TestSQD.git/precache-manifest.9e3a7f5dccf261c57753ca2b0566e46b.js"
+  "/ravimokka/SQProject.git/precache-manifest.cf527da4c35a89cbfd473ad145364f21.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ravimokka/TestSQD.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ravimokka/SQProject.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
